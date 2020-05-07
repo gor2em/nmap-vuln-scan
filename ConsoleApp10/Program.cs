041af2e45a6ff4532d977440b2c3297b886920d3
gor2em <grkem.squash99@gmail.com>
2020-05-07 03:04:05 +0300

    nmap-vuln-scan

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            string dosya = @"output.xml";
+            xmldoc.Load(dosya);
+            xmldoc.Save(Console.Out);
+            Console.ReadKey();
+        }
+    }
+}
